e7d0b17ede5dddc74f41f3bf46d4099e84477978
fahrulrsyd <muhammad.fahrul@if.uai.ac.id>
2021-07-03 19:01:17 +0700

    perbaikan views login dan config base_url

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -13,39 +13,38 @@
     <link href="<?= base_url('assets/') ?>css/style.default.css" rel="stylesheet">
     <script src="<?= base_url('assets/') ?>https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 </head>
+
 <body>
-<form action="<?php echo base_url('login/aksi_login'); ?>" method="post">	
-<div class="container">
-    <form class="form-signin">
-        <p>
-        <h2 class="text-center" class="form-signin-heading">Login</h2>
-        </p>
-        <br>
-        <input type="text" name="username" class="form-control" placeholder="username/email address" autofocus>
-        <input type="password" name="password" class="form-control" placeholder="password">
-        <br>
-        <button class="btn btn-xs btn-primary btn-block" type="submit" href="#">
-            <h4>Login</h4>
-        </button>
-        <br>
-        <div class="text-center">
-            <p>
-            <h5 class="text-center" class="form-signin-heading">Dont have any account?</h5>
-            </p>
-            <button class="btn btn-md btn-primary btn-link" type="submit" href="#">
-                Create Account
-            </button>
+        <div class="container">
+
+            <form class="form-signin" action="<?php echo base_url('login/aksi_login'); ?>" method="post">
+                <h2 class="text-center" class="form-signin-heading">Login</h2>
+                <br>
+                <input type="text" class="form-control" placeholder="Username" autofocus>
+                <input type="password" class="form-control" placeholder="Password">
+
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Login
+                </button>
+                <br>
+                <div class="text-center">
+                    <p>
+                    <h5 class="text-center" class="form-signin-heading">Dont have any account?</h5>
+                    </p>
+                    <button class="btn btn-md btn-primary btn-link" type="submit" href="#">
+                        Create Account
+                    </button>
+                </div>
+            </form>
+
         </div>
 
-    </form>
-</div>
-
-<!-- Bootstrap core JavaScript
+        <!-- Bootstrap core JavaScript
         ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="<?= base_url('assets/') ?>js/jquery-1.10.2.min.js"></script>
-<script src="<?= base_url('assets/') ?>js/bootstrap.min.js"></script>
-</form>
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="<?= base_url('assets/') ?>js/jquery-1.10.2.min.js"></script>
+        <script src="<?= base_url('assets/') ?>js/bootstrap.min.js"></script>
+    </form>
 </body>
 
 </html>
