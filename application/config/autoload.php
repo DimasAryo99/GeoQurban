1c9890a29fb780b0de4830a7ee72145f48729bb2
fahrulrsyd <muhammad.fahrul@if.uai.ac.id>
2021-07-03 19:07:56 +0700

    penambahan 'form_validation' pada autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session');//databasenya dihapus sementara belum ada
+$autoload['libraries'] = array('database', 'session', 'form_validation');//databasenya dihapus sementara belum ada
 
 /*
 | -------------------------------------------------------------------
