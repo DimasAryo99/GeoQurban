f422de9434ca1cc4e52be9283df5ca90049b4f65
DimasAryo99 <82798525+DimasAryo99@users.noreply.github.com>
2021-07-05 21:04:28 +0700

    Penambahan Function CRUD pada controller data_lokasi

--- application/controllers/admin/data_lokasi.php
+++ application/controllers/admin/data_lokasi.php
@@ -5,17 +5,92 @@ class data_lokasi extends CI_Controller
 {
     public function index()
     {
+        $data['masjid'] = $this->lokasi_model->tampil_data()->result();
         $this->load->view('template_admin/header');
         $this->load->view('template_admin/sidebar');
-        $this->load->view('admin/data-masjid');
+        $this->load->view('admin/data-masjid',$data);
         $this->load->view('template_admin/footer');
     }
 
-    public function edit()
+    public function tambahlokasi()
     {
+        $nama_masjid = $this->input->post('nama_masjid');
+        $alamat = $this->input->post('alamat');
+        $deskripsi = $this->input->post('deskripsi');
+        $latitude = $this->input->post('latitude');
+        $longitude = $this->input->post('longitude');
+        $no_telp = $this->input->post('no_telp');
+        $foto = $_FILES['foto']['name'];
+        if ($foto = '') 
+        {
+        } else 
+        {
+            $config['upload_path'] = './uploads';
+            $config['allowed_type'] = 'jpg|jpeg|png|gif';
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('foto')) 
+            {
+                echo "Gambar Gagal di Upload";
+            } else {
+                $foto = $this->upload->data('file_name');
+            }
+        }
+
+        $data = [
+            'nama_masjid'      => $nama_masjid,
+            'alamat'    => $alamat,
+            'deskripsi_masjid'      => $deskripsi,
+            'latitude'         => $latitude,
+            'longitude'          => $longitude,
+            'no_telp'        => $no_telp,
+            'foto'        => $foto
+        ];
+
+        $this->lokasi_model->tambah_lokasi($data, 'data_masjid');
+        redirect('admin/data_lokasi/index');
+    }
+
+    public function editlokasi($id)
+    {
+        $where = ['id_masjid' => $id];
+        $data['masjid'] = $this->lokasi_model->edit_lokasi($where, 'data_masjid')->result();
         $this->load->view('template_admin/header');
         $this->load->view('template_admin/sidebar');
-        $this->load->view('admin/edit-data-lokasi');
+        $this->load->view('admin/edit-data-lokasi', $data);
         $this->load->view('template_admin/footer');
+    }
+
+    public function updatelokasi()
+    {
+        $id = $this->input->post('id_masjid');
+        $nama_masjid = $this->input->post('nama_masjid');
+        $alamat = $this->input->post('alamat');
+        $deskripsi = $this->input->post('deskripsi');
+        $latitude = $this->input->post('latitude');
+        $longitude = $this->input->post('longitude');
+        $no_telp = $this->input->post('no_telp');
+
+        $data = array(
+            'nama_masjid'      => $nama_masjid,
+            'alamat'           => $alamat,
+            'deskripsi_masjid' => $deskripsi,
+            'latitude'         => $latitude,
+            'longitude'        => $longitude,
+            'no_telp'          => $no_telp,
+        );
+
+        $where = array(
+            'id_masjid'     => $id
+        );
+
+        $this->lokasi_model->update_lokasi($where, $data, 'data_masjid');
+        redirect('admin/data_lokasi/index');
+    }
+
+    public function hapuslokasi($id)
+    {
+        $where = array('id_masjid' => $id);
+        $this->lokasi_model->hapus_lokasi($where, 'data_masjid');
+        redirect('admin/data_lokasi/index');
     }
 }
