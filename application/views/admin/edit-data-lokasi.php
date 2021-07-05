694c65a196d4c32877ba655c70a825c4da133adb
DimasAryo99 <82798525+DimasAryo99@users.noreply.github.com>
2021-07-05 21:08:23 +0700

    Integrasi view edit dengan controller data-lokasi

--- application/views/admin/edit-data-lokasi.php
+++ application/views/admin/edit-data-lokasi.php
@@ -3,36 +3,39 @@
         <br>
         <h3><i class="fa fa-edit mb-3"></i> Edit Data Masjid</h3>
 
-        <form method="POST" action="">
+        <?php foreach ($masjid as $m) :  ?>
+
+        <form method="POST" action="<?= base_url('admin/data_lokasi/editlokasi') ?>">
             <div class="for-group mb-2">
                 <label>Nama Masjid</label>
-                <input type="text" name="" class="form-control" value="">
+                <input type="text" name="nama_masjid" class="form-control" value="<?= $m->nama_masjid ?>">
             </div>
             <div class="for-group mb-2">
                 <label>Alamat</label>
-                <input type="hidden" name="" class="form-control" rows="3" value="">
-                <input type="text" name="" class="form-control" rows="3" value="">
+                <input type="text" name="alamat" class="form-control" rows="3" value="<?= $m->alamat ?>">
             </div>
             <div class="for-group mb-2">
                 <label>Deskripsi</label>
-                <input type="text" name="" class="form-control" value="">
+                <input type="text" name="deskripsi" class="form-control" value="<?= $m->deskripsi_masjid ?>">
             </div>
             <div class="for-group mb-2">
                 <label>Latitide</label>
-                <input type="text" name="" class="form-control" value="">
+                <input type="text" name="latitude" class="form-control" value="<?= $m->latitude ?>">
             </div>
             <div class="for-group mb-2">
                 <label>Longitude</label>
-                <input type="text" name="" class="form-control" value="">
+                <input type="text" name="longitude" class="form-control" value="<?= $m->longitude ?>">
             </div>
             <div class="for-group mb-2">
                 <label>No. Telepon</label>
-                <input type="text" name="" class="form-control" value="">
+                <input type="text" name="no_telp" class="form-control" value="<?= $m->no_telp ?>">
             </div>
             <br>
             <button type="button" class="btn btn-secondary">Close</button>
             <button type="submit" class="btn btn-primary ">Save changes</button>
         </form>
+
+        <?php endforeach; ?>
 
     </div>
 </div>
