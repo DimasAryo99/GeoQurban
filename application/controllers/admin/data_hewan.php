a3b3c632d128d49441e6dc5be11a6f98f6a3be4a
Muhammad Ikbar Fahreza <86713537+mikbarf@users.noreply.github.com>
2021-07-06 15:46:30 +0700

    Penambahan controller utk tambah hewan

--- application/controllers/admin/data_hewan.php
+++ application/controllers/admin/data_hewan.php
@@ -10,12 +10,43 @@ class data_hewan extends CI_Controller
         $this->load->view('template_admin/footer');
         $this->load->view('admin/data-hewan');
     }
+	
+	public function tambahhewan()
+    {
+        $jenis_hewan = $this->input->post('jenis_hewan');
+        $jumlah_hewan = $this->input->post('jumlah_hewan');
+        $foto = $_FILES['foto']['name'];
+        if ($foto) 
+        {
+            $config['upload_path'] = './uploads';
+            $config['allowed_type'] = 'jpg|jpeg|png';
+            $this->load->library('upload', $config);
+
+            if ($this->upload->do_upload('foto')) 
+            {
+                $foto = $this->upload->data('file_name');
+            } 
+            else 
+            {
+                echo $this->upload->display_errors();
+            }
+        }
+
+        $data = [
+            'jenis_hewan'      => $jenis_hewan,
+            'jumlah_hewan'    => $jumlah_hewan,
+            'foto'        => $foto,
+        ];
+
+        $this->m_hewan_model->tambah_hewan($data, 'data_hewan');
+        redirect('admin/data_hewan/index');
+    }
 
     public function edithewan()
     {
         $this->load->view('template_admin/header');
         $this->load->view('template_admin/sidebar');
-        $this->load->view('admin/edit-data-hewan');
+        $this->load->view('admin/edit-data-hewan', $data);
         $this->load->view('template_admin/footer');
     }
 
