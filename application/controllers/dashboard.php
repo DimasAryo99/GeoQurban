c836cffc5f2b4751fa27af6579844557fe7e1fad
Muhammad Ikbar Fahreza <86713537+mikbarf@users.noreply.github.com>
2021-07-10 19:18:30 +0700

    Penambahan controller sidebar

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -5,9 +5,10 @@ class dashboard extends CI_Controller
 {
     public function index()
     {
+		$data['user'] = $this->model_data->tampil_data()->result();
         $this->load->view('template_user/header');
         $this->load->view('template_user/topbar');
-        $this->load->view('template_user/sidebar');
+        $this->load->view('template_user/sidebar', $data);
         $this->load->view('template_user/home');
     }
 }
