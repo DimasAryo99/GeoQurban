eb669c7825b2d1653a69ec37038a877c27b1deea
DimasAryo99 <82798525+DimasAryo99@users.noreply.github.com>
2021-07-30 01:28:27 +0700

    Integrasi register dan login admin masjid dengan view

--- application/controllers/login_admin.php
+++ application/controllers/login_admin.php
@@ -1,55 +1,71 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login_Admin extends CI_Controller
-{ //ci_controler class untuk contrlernya
+class login_admin extends CI_Controller 
+{
+    public function index()
+    {    
+        $this->form_validation->set_rules('email','Email','trim|required|valid_email');
+        $this->form_validation->set_rules('password','Password','trim|required');
+        if ($this->form_validation->run() == false)
+        {
+            $this->load->view('auth/login_admin');
+        } 
+        else
+        {
+            //validasi succes
+            $this->_login();
+        }        
+    }
 
-	function __construct()
-	{ //construct disitu nanti yang bakal di jalanin 
-		parent::__construct();
-		$this->load->model('m_login'); // dikomentarin dulu soalnya mau test view
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        
+        $admin = $this->db->get_where('admin_masjid', ['email' => $email])->row_array();
+        
+        //jika usernya ada
+        if($admin) 
+        {
+            //jika usernya aktif
+            if($admin['is_active']==1) 
+            {
+                //cek password
+                if(password_verify($password, $admin['password']))
+                {
+                    $data = ['email' => $admin['email'],
+                    ];
+                    $this->session->set_userdata($data);
+					redirect('login_admin');
+                }
+                else
+                {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Wrong Password</div>');
+                    redirect('login_admin');
+                }
+            }
+            else
+            {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Email has not been activated</div>');
+                redirect('login_admin');
+            }
+        }
+        else
+        {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Email is not registered</div>');
+            redirect('login_admin');
+        }
+    }
 
-	}
-
-	function index()
-	{
-		$this->load->view('auth/login_admin');
-	}
-
-	function aksilogin()
-	{
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		$where = array(
-			'username' => $username,
-			'password' => ($password)
-		);
-		$cek = $this->m_login->cek_login("admin", $where)->num_rows();
-		if ($cek > 0) {
-
-			$data_session = array(
-				'nama' => $username,
-				'status' => "login_admin"
-			);
-
-			$this->session->set_userdata($data_session);
-
-			redirect("admin/dashboard_admin");
-		} else {
-			echo "<script>alert('Username dan Pasword Salah!');</script>";
-			redirect('login_admin', 'refresh');
-		}
-	}
-
-	public function logout()
-	{
-		$this->session->sess_destroy();
-		$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out!</div>');
-		redirect('login_admin/index');
-	}
-	// public function logout()
-	// {
-	//     $this->session->unset_userdata('username');
-	//     $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out!</div>');
-	//     redirect('login');
-	// }
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+        Your have been logout</div>');
+        redirect('login_admin');
+    }
 }
--- application/controllers/registrasi_admin.php
+++ application/controllers/registrasi_admin.php
@@ -5,45 +5,11 @@ class registrasi_admin extends CI_Controller
 {
     public function index()
     {
-        $this->form_validation->set_rules('nama_lengkap','Nama Lengkap','required|trim');
         $this->form_validation->set_rules('email','Email','required|trim|valid_email|is_unique[admin.email]', [
             'is_unique' => 'Email has already registered!']);
-        $this->form_validation->set_rules('password1','Password','required|trim|min_length[3]|matches[password2]');
-        $this->form_validation->set_rules('password2','Password','required|trim|matches[password1]', [
-            'matches' =>  'Password dont match!',
-            'min_length' => 'Password too shot!'
-        ]);
-
-        if ($this->form_validation->run() == false)  
-        {
-            $this->load->view('auth/register');
-        } 
-        else {
-         $data = ['nama_admin' => $this->input->post('nama_lengkap'),
-                  //'telp' => $this->input->post('no_telp'),
-                  'email' => $this->input->post('email'),
-                  //'username' => $this->input->post('username'),
-                  'password' => $this->input->post('password1'),
-                  'is_active' => 0
-                ];
-            $this->db->insert('admin_masjid', $data);
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
-            Your account has been created</div>');
-            redirect('login');
-        }
-    }
-//method terbaru
-    /*
-    <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class registrasi_admin extends CI_Controller 
-{
-    public function index()
-    {
-        $this->form_validation->set_rules('nama_lengkap','Nama Lengkap','required|trim');
-        $this->form_validation->set_rules('email','Email','required|trim|valid_email|is_unique[admin.email]', [
-            'is_unique' => 'Email has already registered!']);
+        $this->form_validation->set_rules('nama_admin','Nama Lengkap','required|trim');
+        $this->form_validation->set_rules('no_telp','Nama Lengkap','required|trim');
+        $this->form_validation->set_rules('username','Nama Lengkap','required|trim');
         $this->form_validation->set_rules('password1','Password','required|trim|min_length[3]|matches[password2]');
         $this->form_validation->set_rules('password2','Password','required|trim|matches[password1]', [
             'matches' =>  'Password dont match!',
@@ -56,9 +22,10 @@ class registrasi_admin extends CI_Controller
         } 
         else {
         $email=$this->input->post('email');
-        $data = ['nama_admin' => $this->input->post('nama_lengkap'),
+        $data = ['nama_admin' => $this->input->post('nama_admin'),
+                 'no_telp' => $this->input->post('no_telp'),
                   'email' => $email,
-                  //'username' => $this->input->post('username'),
+                  'username' => $this->input->post('username'),
                   'password' => $this->input->post('password1'),
                   'is_active' => 0
                 ];
@@ -79,7 +46,7 @@ class registrasi_admin extends CI_Controller
 
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
             Please Activate your account</div>');
-            redirect('login');
+            redirect('login_admin');
         }
     }
 
@@ -143,7 +110,7 @@ class registrasi_admin extends CI_Controller
 
                         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
                         Your account is activated</div>');
-                        redirect('login');
+                        redirect('login_admin');
                     }
                     else
                     {
@@ -152,25 +119,22 @@ class registrasi_admin extends CI_Controller
 
                         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
                Account activation failed! token expired.</div>');
-                redirect('login');
+                redirect('login_admin');
                     }
                 }
                 else 
                 {
                     $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
                Account activation failed! wrong token.</div>');
-                redirect('login');
+                redirect('login_admin');
                 }
              }             
              else
              {
                 $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
                Account activation failed! worng email.</div>');
-                redirect('login');
+                redirect('login_admin');
 
              }
-         }
     }
-    */
-
 }
