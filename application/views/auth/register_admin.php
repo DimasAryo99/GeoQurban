5a065a03b3897f09cd320b39482f24fe5b849958
fahrulrsyd <muhammad.fahrul@if.uai.ac.id>
2021-07-31 18:40:54 +0700

    tampilan registrasi admin masjid (template baru))

--- application/views/auth/register_admin.php
+++ application/views/auth/register_admin.php
@@ -1,125 +1,177 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <title>GeoQurban</title>
-    <meta name="author" content="luckynvic@gmail.com">
-    <link href="<?= base_url('assets/')  ?>css/bootstrap.min.css" rel="stylesheet">
-
-    <link href="<?= base_url('assets/')  ?>ext/customScroll/css/jquery.mCustomScrollbar.css" rel="stylesheet">
-    <link href="<?= base_url('assets/')  ?>css/style.default.css" rel="stylesheet">
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    <title>Registrasi Admin Masjid GeoQurban</title>
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Font-->
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/regis/') ?>css/opensans-font.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/regis/') ?>fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
+    <!-- Main Style Css -->
+    <link rel="stylesheet" href="<?= base_url('assets/regis/') ?>css/style.css" />
 </head>
 
 <body>
-    <nav class="navbar navbar-default nav-fixed-top" role="navigation" id="app-nav-bar">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">GeoQurban</a>
-        </div>
+    <div class="page-content">
+        <div class="form-v1-content">
+            <div class="wizard-form">
+                <form class="form-register" action="<?= base_url('registrasi_admin/index'); ?>" method="post">
+                    <div id="form-total">
+                        <!-- SECTION 1 -->
+                        <h2>
+                            <p class="step-icon"><span>01</span></p>
+                            <span class="step-text">Data Pribadi</span>
+                        </h2>
+                        <section>
+                            <div class="inner">
+                                <div class="wizard-header">
+                                    <h3 class="heading">Informasi Data Admin Masjid</h3>
+                                    <p>Silakan masukkan informasi Anda dan lanjutkan ke langkah berikutnya sehingga kami dapat membuat akun Anda. </p>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder form-holder-2">
+                                        <fieldset>
+                                            <legend>Nama Lengkap</legend>
+                                            <input type="text" class="form-control" name="nama_lengkap" id="nama_lengkap" placeholder="" autofocus>
+                                            <?= form_error('nama_lengkap', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder form-holder-2">
+                                        <fieldset>
+                                            <legend>Your Email</legend>
+                                            <input type="text" class="form-control" name="email" id="email" placeholder="" autofocus>
+                                            <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder form-holder-2">
+                                        <fieldset>
+                                            <legend>Username</legend>
+                                            <input type="text" class="form-control" name="username" id="username" placeholder="" autofocus>
+                                            <?= form_error('username', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder form-holder-2">
+                                        <fieldset>
+                                            <legend>Phone Number</legend>
+                                            <input type="text" class="form-control" name="no_telp" id="no_telp" placeholder="" autofocus>
+                                            <?= form_error('no_telp', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder">
+                                        <fieldset>
+                                            <legend>New Password</legend>
+                                            <input type="password" class="form-control" name="password1" id="password1" placeholder="">
+                                            <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                    <div class="form-holder">
+                                        <fieldset>
+                                            <legend>Confrim Password</legend>
+                                            <input type="password" class="form-control" name="password2" id="password2" placeholder="">
+                                        </fieldset>
+                                    </div>
+                                </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <!--ul class="nav navbar-nav">
-                <li class="active">
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="browse.html">Browse</a>
-                </li>
 
-            </!--ul-->
-            <!--ul class="nav navbar-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Application<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#">Setting</a>
-                        </li>
-                        <li>
-                            <a href="#">Manage User</a>
-                        </li>
+                            </div>
+                        </section>
 
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Master<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#">Point</a>
-                        </li>
-                        <li>
-                            <a href="#">Category</a>
-                        </li>
+                        <!------------------------------------------------------ SECTION 2 ------------------------------------------------------->
+                        <h2>
+                            <p class="step-icon"><span>02</span></p>
+                            <span class="step-text">Data Masjid</span>
+                        </h2>
+                        <section>
+                            <div class="inner">
+                                <div class="wizard-header">
+                                    <h3 class="heading">Informasi Data Masjid</h3>
+                                    <p>Silakan masukkan informasi Anda dan lanjutkan ke langkah berikutnya sehingga kami dapat membuat akun Anda. </p>
+                                </div>
 
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-
-            </!--ul-->
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="<?= base_url("login_admin/index") ?>">Login</a>
-                </li>
-
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </nav>
-    <style>
-        .scroll {
-            height: 300px;
-            width: 300px;
-            overflow: scroll;
-        }
-    </style>
-
-    <div class="container">
-
-        <form class="form-signin" method="post" action="<?= base_url('registrasi_admin/index'); ?>">
-            <div class="text-center">
-                <h2 class="form-signin-heading">Registrasi Admin</h2>
+                                <div class="form-row">
+                                    <div class="form-holder form-holder-2">
+                                        <fieldset>
+                                            <legend>Nama Masjid</legend>
+                                            <input type="text" class="form-control" name="nama_lengkap" id="nama_lengkap" placeholder="" autofocus>
+                                            <?= form_error('nama_masjid', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder form-holder-2">
+                                        <fieldset>
+                                            <legend>Deskripsi Masjid</legend>
+                                            <input type="text" class="form-control" name="deskripsi_masjid" id="deskripsi_masjid" placeholder="" autofocus>
+                                            <?= form_error('deskripsi_masjid', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder form-holder-2">
+                                        <fieldset>
+                                            <legend>Alamat Masjid</legend>
+                                            <input type="text" class="form-control" name="alamat" id="alamat" placeholder="" autofocus>
+                                            <?= form_error('alamat', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder form-holder-2">
+                                        <fieldset>
+                                            <legend>Phone Number</legend>
+                                            <input type="text" class="form-control" name="no_telp" id="no_telp" placeholder="" autofocus>
+                                            <?= form_error('no_telp', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder">
+                                        <fieldset>
+                                            <legend>Longitude</legend>
+                                            <input type="text" class="form-control" name="longitude" id="longitude" placeholder="" autofocus>
+                                            <?= form_error('longitude', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                    <div class="form-holder">
+                                        <fieldset>
+                                            <legend>Latitude</legend>
+                                            <input type="text" class="form-control" name="latitude" id="latitude" placeholder="" autofocus>
+                                            <?= form_error('latitude', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-holder form-holder-2">
+                                        <fieldset>
+                                            <legend>Upload Foto Masjid</legend>
+                                            <div class="custom-file">
+                                                <!-- <input type="fie" class="custom-file-input" id="inputGroupFile01" name="" placeholder="" required> -->
+                                                <input type="file" class="custom-file-input" id="inputGroupFile01">
+                                                <!-- <label class="custom-file-label" for="inputGroupFile01">Choose file</label> -->
+                                            </div>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </form>
             </div>
-                <input type="text" class="form-control" name="email" id="email" placeholder="email" autofocus>
-                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
-
-                <input type="text" class="form-control" name="nama_admin" id="nama_admin" placeholder="nama admin" autofocus>
-                <?= form_error('nama_admin', '<small class="text-danger pl-3">', '</small>'); ?>
-                
-                <input type="text" class="form-control" name="no_telp" id="no_telp" placeholder="nomor telepon" autofocus>
-                <?= form_error('no_telp', '<small class="text-danger pl-3">', '</small>'); ?>
-             
-                <input type="text" class="form-control" name="username" id="username" placeholder="username" autofocus>
-                <?= form_error('username', '<small class="text-danger pl-3">', '</small>'); ?>
-                
-                <input type="password" class="form-control" name="password1" id="password1" placeholder="password">
-                <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
-
-                <input type="password" class="form-control" name="password2" id="password2" placeholder="repeat password">
-
-                <button class="btn btn-lg btn-primary btn-block" type="submit">
-                    Register
-                </button>
-        </form>
-
+        </div>
     </div>
-
-    <!-- Bootstrap core JavaScript
-        ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?= base_url('assets/')  ?>js/jquery-1.10.2.min.js"></script>
-    <script src="<?= base_url('assets/')  ?>js/bootstrap.min.js"></script>
-
-</body>
+    <script src="<?= base_url('assets/regis/') ?>js/jquery-3.3.1.min.js"></script>
+    <script src="<?= base_url('assets/regis/') ?>js/jquery.steps.js"></script>
+    <script src="<?= base_url('assets/regis/') ?>js/main.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
 </html>
