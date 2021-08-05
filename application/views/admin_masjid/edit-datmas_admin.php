b4448191171f59cef960e224c8a4260cd8257976
Muhammad Ikbar Fahreza <86713537+mikbarf@users.noreply.github.com>
2021-08-05 13:55:42 +0700

    tampilan crud data masjid di admin masjid
    Masih ada kendala saat klik btn save dan close (error), tpi data berhasil di edit.

---
+++ application/views/admin_masjid/edit-datmas_admin.php
@@ -0,0 +1,44 @@
+<div class="page-wrapper">
+    <div class="container-fluid">
+        <br>
+        <h3><i class="fa fa-edit mb-3"></i> Edit Data Masjid</h3>
+
+        <?php foreach ($masjid as $m) :  ?>
+
+        <form method="POST" action="<?= base_url() . 'admin_masjid/datmas_admin/updatemasjid' ?>">
+            <div class="for-group mb-2">
+                <input type="text" name="id_masjid" class="id_masjid" value="<?= $m->id_masjid ?>" hidden="">
+            </div>
+            <div class="for-group mb-2">
+                <label>Nama Masjid</label>
+                <input type="text" name="nama_masjid" class="form-control" value="<?= $m->nama_masjid ?>">
+            </div>
+            <div class="for-group mb-2">
+                <label>Alamat</label>
+                <input type="text" name="alamat" class="form-control" rows="3" value="<?= $m->alamat ?>">
+            </div>
+            <!--div class="for-group mb-2">
+                <label>Deskripsi</label>
+                <input type="text" name="deskripsi" class="form-control" value="<?= $m->deskripsi_masjid ?>">
+            </!--div-->
+            <div class="for-group mb-2">
+                <label>Latitide</label>
+                <input type="text" name="latitude" class="form-control" value="<?= $m->latitude ?>">
+            </div>
+            <div class="for-group mb-2">
+                <label>Longitude</label>
+                <input type="text" name="longitude" class="form-control" value="<?= $m->longitude ?>">
+            </div>
+            <div class="for-group mb-2">
+                <label>No. Telepon</label>
+                <input type="text" name="no_telp" class="form-control" value="<?= $m->no_telp ?>">
+            </div>
+            <br>
+            <a href='<?= base_url('admin_masjid/datmas_admin/index') ?>' type="button" class="btn btn-danger">Close</a>
+            <button type="submit" class="btn btn-primary ">Save changes</button>
+        </form>
+
+        <?php endforeach; ?>
+
+    </div>
+</div>
